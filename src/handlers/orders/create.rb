bf985571d90e0dceaece041b84c1b75c068795d7
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2021-05-30 21:35:50 +0200

    Add possiblity to save orders to the DynamoDB
    - Added aws-sdk-dynamodb gem
    - Cleanup package.json
    - Added serverless-pseudo-parameters
    - Added resources section for DynamoDB table generation
    - Added DynamoDB put action to create function

--- src/handlers/orders/create.rb
+++ src/handlers/orders/create.rb
@@ -2,6 +2,10 @@
 
 require 'json'
 require 'date'
+require 'securerandom'
+require 'aws-sdk-dynamodb'
+
+dynamodb_client = Aws::DynamoDB::Client.new
 
 def create(event:, context:)
   body = JSON.parse(event["body"])
@@ -9,10 +13,18 @@ def create(event:, context:)
   date = DateTime.now.iso8601
 
   order = {
+    id: SecureRandom.uuid,
     name: name,
     status: 'INCOMING_ORDER',
     created_at: date
   }
+
+  table_item = {
+    table_name: 'OrdersTable',
+    item: order
+  }
+
+  dynamodb_client.put_item(table_item)
 
   { statusCode: 201, body: order.to_json }
 end
